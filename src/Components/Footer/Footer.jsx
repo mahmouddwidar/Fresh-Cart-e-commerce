75d541325c2b2df445b6df3ad00fcc5dc79a017a
Mahmoud Dwidar <mahmouddwidar55@gmail.com>
2025-03-02 00:47:46 +0200

    Feat: fetched products data in home page with axios and completed the footer styles

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,12 @@
 import { useFormik } from "formik";
+import { Link } from "react-router-dom";
 import * as Yup from "yup";
 import Style from "./Footer.module.css";
+import amazonPay from "../../assets/footer/Amazon_Pay_logo.png";
+import masterCard from "../../assets/footer/MasterCard-Logo.png";
+import paypal from "../../assets/footer/PayPal.png";
+import appleStore from "../../assets/footer/get-it-on-apple-store.png";
+import googlePlay from "../../assets/footer/get-it-on-google-play-badge.png";
 
 export default function Footer() {
 	const validationSchema = Yup.object({
@@ -24,7 +30,10 @@ export default function Footer() {
 				<p>
 					We will send you a link, open it on your phone to download the app.
 				</p>
-				<form onSubmit={formik.handleSubmit} className="ps-4 d-flex justify-content-between align-items-center">
+				<form
+					onSubmit={formik.handleSubmit}
+					className="ps-4 mb-4 d-flex justify-content-between align-items-center"
+				>
 					<input
 						type="email"
 						name="email"
@@ -41,6 +50,50 @@ export default function Footer() {
 				{formik.errors.email && formik.touched.email && (
 					<p className="alert alert-danger mx-4 mt-4">{formik.errors.email}</p>
 				)}
+				<hr className="opacity-25" />
+				<div className="d-flex justify-content-between align-items-center">
+					<div className="d-flex justify-content-between align-items-center">
+						<h5 className="me-3">Payments Partners</h5>
+						<img
+							className="me-2"
+							style={{ width: "48px" }}
+							src={amazonPay}
+							alt="Amazon Pay Logo"
+						/>
+						<img
+							className="me-2"
+							style={{ width: "48px" }}
+							src={masterCard}
+							alt="Master Card Logo"
+						/>
+						<img
+							className="me-2"
+							style={{ width: "48px" }}
+							src={paypal}
+							alt="Paypal Logo"
+						/>
+					</div>
+					<div className="d-flex justify-content-between align-items-center">
+						<h5 className="me-3">Get deliveries with FreshCart</h5>
+						<Link to={"/"}>
+							<img
+								className="me-2"
+								style={{ width: "98px" }}
+								src={appleStore}
+								alt="Apple Store Logo"
+							/>
+						</Link>
+						<Link to={"/"}>
+							<img
+								className="me-2"
+								style={{ width: "108px" }}
+								src={googlePlay}
+								alt="Google Play Logo"
+							/>
+						</Link>
+					</div>
+				</div>
+				<hr className="mb-4 opacity-25" />
 			</footer>
 		</section>
 	);
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,65 @@
+import { useEffect, useState } from "react";
 import Style from "./Home.module.css";
+import axios from "axios";
+import { Grid } from "react-loader-spinner";
+import { Link } from "react-router-dom";
 
 export default function Home() {
+	const [products, setProducts] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	async function getAllProducts() {
+		setIsLoading(true);
+		await axios
+			.get("https://ecommerce.routemisr.com/api/v1/products")
+			.then((response) => {
+				setProducts(response.data.data);
+				console.log(products);
+			})
+			.catch((error) => {
+				console.error("error happened while getting products: ", error);
+			});
+		setIsLoading(false);
+	}
+
+	useEffect(() => {
+		getAllProducts();
+	}, []);
+
 	return (
-		<>
-			<h1>Home</h1>
-		</>
+		<section className="container d-flex justify-content-center align-items-center" style={{ minHeight: '80vh'}}>
+			{isLoading ? (
+				<div className="d-flex justify-content-center align-items-center">
+					<Grid
+						visible={true}
+						height="80"
+						width="80"
+						color="#4fa94d"
+						ariaLabel="grid-loading"
+						radius="12.5"
+						wrapperStyle={{}}
+						wrapperClass="grid-wrapper"
+					/>
+				</div>
+			) : (
+				<div className="d-flex justify-content-between align-items-center flex-wrap">
+					{products.map((product, index) => {
+						return (
+							<Link to={`products`} className="card text-decoration-none col-md-2 m-2" key={index}>
+								<div className=" overflow-hidden" style={{ height: "200px" }}>
+									<img
+										src={product.imageCover}
+										alt={product.title}
+										className="w-100"
+									/>
+								</div>
+								<h6 className=" mt-2 text-success">{product.category.name}</h6>
+								<h4>{product.title}</h4>
+							</Link>
+						);
+					})}
+				</div>
+			)}
+		</section>
 	);
 }
