84ac2de7e83833b06a70bca061fda50b9987eadf
Mahmoud Dwidar <mahmouddwidar55@gmail.com>
2025-03-07 22:57:00 +0200

    feat: Added offline status indicator using react-detect-offline library - Added a styled offline message with a border and warning icon for better user feedback.

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -4,6 +4,7 @@ import Footer from "../Footer/Footer";
 import Style from "./Layout.module.css";
 import { useContext, useEffect } from "react";
 import { UserContext } from "../../Context/UserContext";
+import { Offline } from "react-detect-offline";
 
 export default function Layout() {
 	let { setUserToken } = useContext(UserContext);
@@ -19,6 +20,15 @@ export default function Layout() {
 			<Navbar />
 			<div className="container">
 				<Outlet />
+			</div>
+			<div>
+				<Offline>
+					<div className={`${Style["network-offline"]} border border-danger`}>
+						{" "}
+						<i className="fa-solid fa-wifi text-danger me-2"></i> You&apos;re
+						currently offline
+					</div>
+				</Offline>
 			</div>
 			<Footer />
 		</>
