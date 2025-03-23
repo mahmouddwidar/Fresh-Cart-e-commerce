eb40eb24b0123e45bbf4e7135ab1840c65b9d0f8
Mahmoud Dwidar <mahmouddwidar55@gmail.com>
2025-03-23 02:35:05 +0200

    feat: add custom offline detection hook. - removed  library and made a custom hook to replace it. - moved the css styles from Layout module to .

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-import Style from "./Layout.module.css";
+// import Style from "./Layout.module.css";
 import { useContext, useEffect } from "react";
 import { UserContext } from "../../Context/UserContext";
-import { Offline } from "react-detect-offline";
+// import { Offline } from "react-detect-offline";
+import useOfflineDetection from "../../Hooks/useOfflineDetection";
 
 export default function Layout() {
 	let { setUserToken } = useContext(UserContext);
+	let detectOffline = useOfflineDetection();
 
 	useEffect(() => {
 		if (localStorage.getItem("userToken") !== null) {
@@ -18,18 +20,10 @@ export default function Layout() {
 	return (
 		<>
 			<Navbar />
-			<div className="container" style={{ minHeight: '70vh' }}>
+			<div className="container" style={{ minHeight: "70vh" }}>
 				<Outlet />
 			</div>
-			<div>
-				<Offline>
-					<div className={`${Style["network-offline"]} border border-danger`}>
-						{" "}
-						<i className="fa-solid fa-wifi text-danger me-2"></i> You&apos;re
-						currently offline
-					</div>
-				</Offline>
-			</div>
+			{detectOffline}
 			<Footer />
 		</>
 	);
